35e1e6d3c62ceb25a43d6d1ac73bb932f542e482
Yassine <yassinedaho25@gmail.com>
2023-02-10 14:33:01 +0100

    troisième commit

---
+++ animation.js
@@ -0,0 +1,10 @@
+ <canvas class="particles-js-canvas-el" width="1669" height="969" style="width: 100%; height: 100%;"></canvas>
+
+
+.particles-box .particles-js-canvas-el {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
